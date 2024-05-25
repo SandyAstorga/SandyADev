6d60cb3c355ec08b102370ed999a588121f1370c
SandyAstorga <sandyastorga13@gmail.com>
2024-05-25 12:22:53 -0600

    feat():nuevas-funcionalidades

--- src/posts/posts.js
+++ src/posts/posts.js
@@ -1,11 +1,10 @@
-
 export const myPosts = [
     {
         id: 1,
         category: 'Videojuegos',
         subCategory: 'Javascript',
         type: 'secundario',
-        title: '¿Por que aprender a programar?',
+        title: '¿Por qué aprender a programar?',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         date: 'Mayo 20, 2024',
         image: "https://a.storyblok.com/f/96536/1280x720/f87e0fe23e/javascript-react-portada.jpg"
@@ -15,7 +14,7 @@ export const myPosts = [
         category: 'Videojuegos',
         subCategory: 'Javascript',
         type: 'secundario',
-        title: '¿Que es Javascript?',
+        title: '¿Qué es Javascript?',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         date: 'Marzo 05, 2024',
         image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
@@ -24,7 +23,7 @@ export const myPosts = [
         id: 3,
         category: 'Videojuegos',
         subCategory: 'Javascript',
-        title: 'fdgsrhndc',
+        title: 'Introducción a la Programación',
         type: 'principal',
         content: 'djhfndgmjflm bfdz;l v;ela kvfh vjfdhnz',
         date: 'Marzo 05, 2024',
@@ -34,7 +33,7 @@ export const myPosts = [
         id: 4,
         category: 'Programación',
         subCategory: 'React',
-        title: 'Que es React',
+        title: '¿Qué es React?',
         type: 'secundario',
         content: 'djhfndgmjflm bfdz;l v;ela kvfh vjfdhnz',
         date: 'Marzo 05, 2024',
@@ -45,7 +44,7 @@ export const myPosts = [
         category: 'Videojuegos',
         subCategory: 'Javascript',
         type: 'secundario',
-        title: 'sdfghjkertyui',
+        title: 'Beneficios de aprender Javascript',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         date: 'Mayo 20, 2024',
         image: "https://a.storyblok.com/f/96536/1280x720/f87e0fe23e/javascript-react-portada.jpg"
@@ -55,7 +54,7 @@ export const myPosts = [
         category: 'Videojuegos',
         subCategory: 'sdyfgyuhij',
         type: 'destacado',
-        title: 'Que es la programacion',
+        title: '¿Qué es la programación?',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         date: 'Marzo 05, 2024',
         image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
@@ -64,7 +63,7 @@ export const myPosts = [
         id: 7,
         category: 'Tecnología',
         subCategory: 'CSS',
-        title: 'zx,mxnfzgdmfg',
+        title: 'Introducción a CSS',
         type: 'secundario',
         content: 'djhfndgmjflm bfdz;l v;ela kvfh vjfdhnz',
         date: 'Marzo 05, 2024',
@@ -74,12 +73,110 @@ export const myPosts = [
         id: 8,
         category: 'Tecnología',
         subCategory: 'HTML',
-        title: 'sdlfkgjf;l',
+        title: 'Introducción a HTML',
         type: 'secundario',
         content: 'djhfndgmjflm bfdz;l v;ela kvfh vjfdhnz',
         date: 'Marzo 05, 2024',
         image: "https://a.storyblok.com/f/96536/1280x720/f87e0fe23e/javascript-react-portada.jpg"
+    },
+    {
+        id: 9,
+        category: 'Programación',
+        subCategory: 'React',
+        type: 'principal',
+        title: 'React Hooks',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        date: 'Abril 15, 2024',
+        image: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+    },
+    {
+        id: 10,
+        category: 'Tecnología',
+        subCategory: 'CSS',
+        type: 'secundario',
+        title: 'CSS Flexbox',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        date: 'Febrero 10, 2024',
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+    },
+    {
+        id: 11,
+        category: 'Videojuegos',
+        subCategory: 'Javascript',
+        type: 'principal',
+        title: 'JavaScript ES6 Features',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        date: 'Enero 25, 2024',
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+    },
+    {
+        id: 12,
+        category: 'Programación',
+        subCategory: 'Node.js',
+        type: 'secundario',
+        title: 'Introducción a Node.js',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        date: 'Marzo 15, 2024',
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg"
+    },
+    {
+        id: 13,
+        category: 'Tecnología',
+        subCategory: 'Web Development',
+        type: 'destacado',
+        title: 'Future of Web Development',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        date: 'Abril 25, 2024',
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+    },
+    {
+        id: 14,
+        category: 'Videojuegos',
+        subCategory: 'Development',
+        type: 'secundario',
+        title: 'Game Development with JavaScript',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        date: 'Mayo 01, 2024',
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+    },
+    {
+        id: 15,
+        category: 'Tecnología',
+        subCategory: 'AI',
+        type: 'principal',
+        title: 'Introduction to AI in Tech',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        date: 'Marzo 20, 2024',
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+    },
+    {
+        id: 16,
+        category: 'Programación',
+        subCategory: 'Python',
+        type: 'secundario',
+        title: 'Introduction to Python',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        date: 'Febrero 28, 2024',
+        image: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg"
+    },
+    {
+        id: 17,
+        category: 'Tecnología',
+        subCategory: 'Networking',
+        type: 'secundario',
+        title: 'Basics of Networking',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        date: 'Enero 18, 2024',
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+    },
+    {
+        id: 18,
+        category: 'Videojuegos',
+        subCategory: 'Design',
+        type: 'secundario',
+        title: 'Game Design Principles',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        date: 'Abril 10, 2024',
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
     }
 ];
-
-// console.log(myPosts)
